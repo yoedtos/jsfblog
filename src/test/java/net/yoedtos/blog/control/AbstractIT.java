828bc369d106267344a8d4661fd6f868fd366f97
Edson Yoshimaru <yoedtos@hotmail.com>
2023-08-03 11:52:26 +0900

    feat: reusable methods for IT tests

--- src/test/java/net/yoedtos/blog/control/AbstractIT.java
+++ src/test/java/net/yoedtos/blog/control/AbstractIT.java
@@ -1,7 +1,11 @@
 package net.yoedtos.blog.control;
 
+import static net.yoedtos.blog.util.TestConstants.LOGIN_URI;
+import static net.yoedtos.blog.util.TestConstants.TEST_CONTEXT;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -9,6 +13,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public abstract class AbstractIT {
 
+	protected String[] ids;
+	protected String[] data; 
+			
 	protected WebDriver driver;
 
 	@BeforeClass
@@ -25,5 +32,24 @@ public abstract class AbstractIT {
 		FirefoxOptions options = new FirefoxOptions();
 		options.setHeadless(true);
 		return options;
+	}
+	
+	protected void login(String username, String password) {
+		driver.get(TEST_CONTEXT + LOGIN_URI);
+		driver.findElement(By.id("login:userLogin")).sendKeys(username);
+		driver.findElement(By.id("login:userPassword")).sendKeys(password);	
+		driver.findElement(By.id("login:blogin")).click();
+	}
+	
+	protected void fillForm() {
+		for (int i = 0; i < ids.length; i++) {
+			driver.findElement(By.id(ids[i])).sendKeys(data[i]);
+		}
+	}
+	
+	protected void clearForm() {
+		for (int i = 0; i < ids.length; i++) {
+			driver.findElement(By.id(ids[i])).clear();
+		}
 	}
 }
